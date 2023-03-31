465fadb392bd11bdcfd4d3f569cc51e0c04fecff
lordjoo <lordjoo@users.noreply.github.com>
2023-03-31 15:14:13 +0000

    Fix styling

--- src/ConfigReaders/ConfigDBDriver.php
+++ src/ConfigReaders/ConfigDBDriver.php
@@ -17,6 +17,7 @@ class ConfigDBDriver implements ConfigReaderInterface
     public function getQuery(): Builder
     {
         $table = config('apigee.db.table_name');
+
         return DB::table($table)->newQuery();
     }
 
@@ -25,28 +26,31 @@ class ConfigDBDriver implements ConfigReaderInterface
         $this->data = $this->getQuery()->first()->toArray();
     }
 
-
     public function getOrganization(): string
     {
         $col = config('apigee.db.columns.organization');
+
         return $this->data[$col];
     }
 
     public function getEndpoint(): string
     {
         $col = config('apigee.db.columns.endpoint');
+
         return $this->data[$col];
     }
 
     public function getUserName(): string
     {
         $col = config('apigee.db.columns.username');
+
         return $this->data[$col];
     }
 
     public function getPassword(): string
     {
         $col = config('apigee.db.columns.password');
+
         return $this->data[$col];
     }
 }
